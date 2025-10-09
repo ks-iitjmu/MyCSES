5f91c9685305a16f92cede2e6178fa483122990f
ks-iitjmu <2023uma0221@iitjammu.ac.in>
2025-10-09 22:56:59 +0530

    changes done

--- 2_sorting_searching/distinct_values_subsequences.cpp
+++ 2_sorting_searching/distinct_values_subsequences.cpp
@@ -1,36 +1,31 @@
-// Kunal Sharma IIT Jammu
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
-typedef vector<ll> vll;
-typedef map<ll, ll> umll;
+#define mod 1000000007
 
-const ll mod = 1e9 + 7;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll n;
-    cin >> n;
-    vll a(n);
-    for (ll &x : a)
-        cin >> x;
-    vll dp(n + 1);
-    dp[0] = 1;
-    umll last;
-    for (ll i = 1; i <= n; i++)
-    {
-        dp[i] = (dp[i - 1] * 2) % mod;
-        if (last.count(a[i - 1]))
-        {
-            ll j = last[a[i - 1]];
-            dp[i] = (dp[i] - dp[j - 1] + mod) % mod;
-        }
-        last[a[i - 1]] = i;
+
+    ll n; 
+    if (!(cin >> n)) return 0;
+    unordered_map<ll, ll> freq;
+    freq.reserve((size_t)2 * max(1LL, n));
+
+    for (ll i = 0; i < n; ++i) {
+        ll x; cin >> x;
+        ++freq[x];
     }
-    cout << (dp[n] - 1 + mod) % mod << "\n";
+
+    long long ans = 1;
+    for (auto &kv : freq) {
+        ans = (ans * ((kv.second + 1) % mod)) % mod;
+    }
+    ans = (ans - 1 + mod) % mod;  // remove empty subsequence
+
+    cout << ans << '\n';
     return 0;
 }
