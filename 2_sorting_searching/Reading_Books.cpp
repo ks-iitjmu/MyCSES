670b811806882aa99fc13d2d846cc4dfbd203dcb
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-10-09 21:56:15 +0530

    Add solution

---
+++ 2_sorting_searching/Reading_Books.cpp
@@ -0,0 +1,30 @@
+// Kunal Sharma IIT Jammu
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+#define bg begin()
+#define ed end()
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n;
+    cin >> n;
+    vll t(n);
+    ll sum = 0, max_time = 0;
+    for (ll &x : t)
+    {
+        cin >> x;
+        sum += x;
+        max_time = max(max_time, x);
+    }
+
+    cout << max(sum, 2 * max_time) << "\n";
+
+    return 0;
+}
