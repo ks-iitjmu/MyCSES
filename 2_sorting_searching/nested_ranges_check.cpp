130d090440b87cb06160cf45a0b67d0269960d37
himanshu-maano <himanshu.kumarchy3@gmail.com>
2025-10-10 11:45:10 +0530

    all test case passed

--- 2_sorting_searching/nested_ranges_check.cpp
+++ 2_sorting_searching/nested_ranges_check.cpp
@@ -2,47 +2,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Range {
-    int x, y, index;
+#define ll long long
+
+struct range
+{
+    int left, right, index;
+    bool operator<(const range &other) const
+    {
+        if (left == other.left)
+            return right > other.right;
+        return left < other.left;
+    }
 };
 
-bool compareRanges(const Range& a, const Range& b) {
-    if (a.x == b.x) return a.y > b.y;
-    return a.x < b.x;
-}
-
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
-    vector<Range> ranges(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> ranges[i].x >> ranges[i].y;
-        ranges[i].index = i;
+    vector<range> ranges(n);
+    vector<bool> contained(n);
+    vector<bool> contains(n);
+    for (int indx = 0; indx < n; indx++)
+    {
+        cin >> ranges[indx].left;
+        cin >> ranges[indx].right;
+        ranges[indx].index = indx;
+    }
+    sort(ranges.begin(), ranges.end());
+
+    int maxend = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (ranges[i].right <= maxend)
+            contained[ranges[i].index] = true;
+        maxend = max(maxend, ranges[i].right);
     }
 
-    sort(ranges.begin(), ranges.end(), compareRanges);
-
-    vector<int> contains(n, 0), is_contained(n, 0);
-
-    int max_end = 0;
-    for (int i = 0; i < n; ++i) {
-        if (ranges[i].y <= max_end) is_contained[ranges[i].index] = 1;
-        max_end = max(max_end, ranges[i].y);
+    int minend = INT_MAX;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (ranges[i].right >= minend)
+            contains[ranges[i].index] = true;
+        minend = min(minend, ranges[i].right);
     }
 
-    int min_end = 2e9;
-    for (int i = n - 1; i >= 0; --i) {
-        if (ranges[i].y >= min_end) contains[ranges[i].index] = 1;
-        min_end = min(min_end, ranges[i].y);
+    for (int i = 0; i < n; i++)
+    {
+        cout << contains[i] << " ";
     }
-
-    for (int i = 0; i < n; ++i) cout << contains[i] << (i == n - 1 ? "" : " ");
-    cout << "\n";
-    for (int i = 0; i < n; ++i) cout << is_contained[i] << (i == n - 1 ? "" : " ");
-    cout << "\n";
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << contained[i] << " ";
+    }
+    cout << endl;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
